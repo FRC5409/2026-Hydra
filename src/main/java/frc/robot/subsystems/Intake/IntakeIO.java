04852ce533e4237384d2e290599cbb68a7695e8e
John <75291116+john666max@users.noreply.github.com>
2026-01-15 19:10:53 -0500

    Update IntakeIO.java
    updated IntakeIO

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.Intake;
+
+
+import org.littletonrobotics.junction.AutoLog;
+public interface IntakeIO {
+    @AutoLog
+    public class intakeInputs{
+        public boolean intakeConnection = false;
+        public double intakeVolts = 0.0;
+        public double intakeCurrent = 0.0;
+        public double intakeTemp = 0.0;
+        public double intakeVelocity = 0.0;
+        public double intakePosition = 0.0;
+    }
+    public default void setVoltage(double volts) {}
+    public default void updateInputs(intakeInputs inputs) {}
+    public default double getMotorCurrent() { return 0.0; }
+    public default void coastMode() {}
+    public default void brakeMode() {}
+} 
