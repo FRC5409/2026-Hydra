5e4dafabf46fa15833f2143e8ca7c5f38f7e9b43
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2026-01-17 13:31:36 -0500

    Spark Max indexer for Ortona
    - Prototyping with Ortona as a launcher, needs something to bring the fuel up to the launcher. Just a motor that runs.

---
+++ src/main/java/frc/robot/subsystems/Serializer/SerializerIOSparkMax.java
@@ -0,0 +1,46 @@
+package frc.robot.subsystems.Serializer;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.RobotController;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+
+
+// Indexer for Ortona
+public class SerializerIOSparkMax implements SerializerIO {
+    private SparkMax motor;
+    private SparkMaxConfig motorConfig;
+    private final boolean inverted = false;
+    
+    public SerializerIOSparkMax(int ID) {
+        motor =  new SparkMax(ID, MotorType.kBrushless);
+        motorConfig = new SparkMaxConfig();
+
+        motorConfig.smartCurrentLimit(30);
+        motorConfig.idleMode(IdleMode.kBrake);
+        motorConfig.inverted(inverted);
+
+        motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    @Override
+    public void setMotorVoltage(double voltage){
+        motor.setVoltage(voltage);
+    }
+
+    @Override
+    public void updateInputs(SerializerInputs inputs){
+        inputs.mainMotorConnection = !(motor.getFaults().motorType || motor.getFaults().can);
+        inputs.mainAppliedVoltage = motor.get() * RobotController.getBatteryVoltage();
+        inputs.mainAppliedCurrent = motor.getOutputCurrent();
+        inputs.mainMotorTemperature = motor.getMotorTemperature();
+    }
+
+    
+
+}
