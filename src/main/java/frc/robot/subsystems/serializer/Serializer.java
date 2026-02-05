831770df75de92e1aa621ac9619e44bca743a40e
chzboi42 <1rajanjad@hdsb.ca>
2026-02-05 15:42:31 -0500

    Checkmate testing for indexer & feeder

--- src/main/java/frc/robot/subsystems/serializer/Serializer.java
+++ src/main/java/frc/robot/subsystems/serializer/Serializer.java
@@ -1,11 +1,15 @@
 package frc.robot.subsystems.serializer;
 
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.Checkmate;
+import frc.robot.utils.Checkmate.TestResult;
 
 public class Serializer extends SubsystemBase{
     private SerializerIO io;
@@ -14,6 +18,33 @@ public class Serializer extends SubsystemBase{
     public Serializer(SerializerIO io) {
         this.io = io;
         inputs = new SerializerInputsAutoLogged();
+        
+        Checkmate.register("Indexer spins", () -> {
+            Command cmd = this.setIndexerVoltage(2);
+            cmd.initialize();
+            cmd.execute();
+            if(this.getIndexerVelocity().in(DegreesPerSecond) > 0) {
+                return TestResult.success("Indexer spins the right way");
+            } else if (this.getIndexerVelocity().in(DegreesPerSecond) < 0) {
+                return TestResult.fail("Indexer spins the wrong way");
+            } else {
+                return TestResult.fail("Indexer is not spinning!");
+            }
+        });
+
+        Checkmate.register("Feeder spins", () -> {
+            Command cmd = this.setFeederVoltage(2);
+            cmd.initialize();
+            cmd.execute();
+            if(this.getFeederVelocity().in(DegreesPerSecond) > 0) {
+                return TestResult.success("Feeder spins the right way");
+            } else if (this.getFeederVelocity().in(DegreesPerSecond) < 0) {
+                return TestResult.fail("Feeder spins the wrong way");
+            } else {
+                return TestResult.fail("Feeder is not spinning!");
+            }
+        });
+
     }
 
     public Command setFeederVoltage(double voltage){
