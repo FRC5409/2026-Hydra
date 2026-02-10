394c02b6a7961eb9245b902ad16f13c45c324b21
chzboi42 <1rajanjad@hdsb.ca>
2026-02-10 16:50:13 -0500

    Feeder: Finished setting up sim PID
    - Added pid controller
    - Logging for velocity + rotations
    - Very rough tuning of sim PID

--- src/main/java/frc/robot/subsystems/feeder/FeederIOSim.java
+++ src/main/java/frc/robot/subsystems/feeder/FeederIOSim.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.feeder;
 
 import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
@@ -10,77 +11,75 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 
 public class FeederIOSim implements FeederIO {
-    private double simVoltage = 0.0;
-    private double simCurrent = 0.0;
     private final FlywheelSim feederSim;
     private final DCMotor motor = DCMotor.getKrakenX44(1);
     private final PIDController controller;
     private boolean running;
+    private double numberOfRotations;
 
     public FeederIOSim() {
         feederSim = new FlywheelSim(
             LinearSystemId.createFlywheelSystem(
                         motor,
-                        1,
+                        0.002,
                         1
                 ),
             motor
         );
 
-        feederSim.update(0.01);
         controller = new PIDController(
-            FeederConstants.TALONFX_PID.kP, 
-            FeederConstants.TALONFX_PID.kI,
-            FeederConstants.TALONFX_PID.kD);
+            FeederConstants.SIM_PID.kP, 
+            FeederConstants.SIM_PID.kI,
+            FeederConstants.SIM_PID.kD);
         running = false;
-    }
-
-    private void setAngularVelocity(double velocityRotationsPerSec) {
-        feederSim.setAngularVelocity(velocityRotationsPerSec*(2*Math.PI));
     }
 
     @Override
     public void setMotorVoltage(double voltage) {
-        simVoltage = voltage;
+        feederSim.setInputVoltage(voltage);
+        running = true;
     }
 
     @Override
-    public void runRPS(double velocity) {
-        setAngularVelocity(velocity);
+    public void runRPS(double RPS) {
+        controller.setSetpoint(RPS);
+        running = true;
     }
 
     @Override
     public void stopMotor() {
-        //feederVoltage = 0.0;
-        setAngularVelocity(0);
-        controller.reset();
+        controller.setSetpoint(0.0);
+        running = false;
     }
 
     @Override
     public AngularVelocity getVelocityRPS() {
-        //return RotationsPerSecond.of(feederVoltage);
         return RotationsPerSecond.of(feederSim.getAngularVelocityRPM()/60);
     }
 
     
     @Override
     public void updateInputs(FeederInputs inputs) {
+        double simVoltage = 0.0;
         if (running) {
             simVoltage = MathUtil.clamp(
-                    controller.calculate(feederSim.getAngularVelocityRPM()),
-                    -12,
-                    12
+                    controller.calculate(getVelocityRPS().in(RotationsPerSecond)),
+                    -RoboRioSim.getVInVoltage(),
+                    RoboRioSim.getVInVoltage()
             );
-
-            simCurrent = feederSim.getCurrentDrawAmps();
         }
-
+        feederSim.setInputVoltage(simVoltage);
+        feederSim.update(0.02);
+        
         inputs.isFeederMotorConnected = true;
-        inputs.feederAppliedVoltage = Volts.of(simVoltage);
+        inputs.feederAppliedVoltage = Volts.of(feederSim.getInputVoltage());
         inputs.feederMotorVelocity = getVelocityRPS();
-        inputs.feederAppliedCurrent = Amps.of(simCurrent);
+        inputs.feederAppliedCurrent = Amps.of(feederSim.getCurrentDrawAmps());
+        numberOfRotations += getVelocityRPS().in(RotationsPerSecond)*0.02;
+        inputs.feederMotorPosition = Rotations.of(numberOfRotations);
     }
 
 }
