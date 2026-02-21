615dc69e480d2219689de48604b76e7eb51c00e0
Logan Dhillon <dev@logandhillon.com>
2026-02-21 10:16:05 -0500

    chore: cleanup elevator code

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,9 +1,5 @@
 package frc.robot.subsystems.elevator;
 
-import static edu.wpi.first.units.Units.*;
-
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.units.Units;
@@ -13,27 +9,30 @@ import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.elevator.ElevatorConstants;
+import frc.robot.Constants;
+import org.littletonrobotics.junction.Logger;
 
-public class Elevator extends SubsystemBase{
+import static edu.wpi.first.units.Units.Meters;
 
-    private final ElevatorIO io;
+public class Elevator extends SubsystemBase {
+    private final ElevatorIO               io;
     private final ElevatorInputsAutoLogged inputs;
 
-    private static Pose3d elevatorPose;
+    private Pose3d elevatorPose;
 
     // Setup alerts for elevator motors connection
-    private final Alert ElevatorAlert  = new Alert("The Left Elevator Motor is Disconnected " + ElevatorConstants.MAIN_MOTOR_ID, AlertType.kError);
+    private final Alert ElevatorAlert = new Alert(
+            "Left elevator motor disconnected! CAN#: " + Constants.DeviceID.CLIMBER_MOTOR, AlertType.kError);
 
     public Elevator(ElevatorIO io) {
         this.io = io;
-        inputs = new ElevatorInputsAutoLogged();
-
-        elevatorPose = new Pose3d();
+        this.inputs = new ElevatorInputsAutoLogged();
+        this.elevatorPose = new Pose3d();
     }
 
     /**
      * Sets voltage of elevator to given voltage
+     *
      * @param voltage voltage value
      */
     public Command startManualMove(double voltage) {
@@ -41,33 +40,33 @@ public class Elevator extends SubsystemBase{
     }
 
     /**
-     * Zeros elevator encoder position 
+     * Zeros elevator encoder position
      */
     public Command zeroEncoder() {
-        return Commands.runOnce(() -> io.zeroEncoder(), this);
+        return Commands.runOnce(io::zeroEncoder, this);
     }
 
     /**
-     * Sets the position of the elevator
-     * Command ends when the elevator is within 25mm range of the targeted position
+     * Sets the position of the elevator Command ends when the elevator is within 25mm range of the targeted position
+     *
      * @param setpoint setpoint value
      */
     public Command elevatorGo(Distance setpoint) {
         return Commands.sequence(
-            Commands.runOnce(() -> io.setSetpoint(setpoint), this),
-            Commands.waitUntil(() -> setpoint.isNear(getPosition(), Meters.of(0.025)))
-        );
+                Commands.runOnce(() -> io.setSetpoint(setpoint), this),
+                Commands.waitUntil(() -> setpoint.isNear(getPosition(), Meters.of(0.025))));
     }
 
     /**
-     * Stop all motor 
+     * Stop all motor
      */
     public Command stopAll() {
-        return Commands.runOnce(() -> io.stopMotor(), this);
+        return Commands.runOnce(io::stopMotor, this);
     }
-    
+
     /**
      * Gets position of the elevator
+     *
      * @return The encoders position
      */
     public Distance getPosition() {
@@ -77,13 +76,12 @@ public class Elevator extends SubsystemBase{
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
-        io.updateInputs(inputs);
-        Logger.processInputs("Elevator", inputs);
+        io.updateInputs(inputs); Logger.processInputs("Elevator", inputs);
         Logger.recordOutput("Components/Elevator", elevatorPose);
 
         //Alert if motors are disconnected
         ElevatorAlert.set(!inputs.isMainMotorConnected);
 
-        elevatorPose = new Pose3d(0,0,inputs.mainMotorPosition.in(Units.Meters), new Rotation3d());
+        elevatorPose = new Pose3d(0, 0, inputs.mainMotorPosition.in(Units.Meters), new Rotation3d());
     }
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -1,45 +1,42 @@
 package frc.robot.subsystems.elevator;
 
-import static edu.wpi.first.units.Units.Inches;
-import static edu.wpi.first.units.Units.Kilograms;
-import static edu.wpi.first.units.Units.Meters;
-import edu.wpi.first.units.Units;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj.simulation.RoboRioSim;
-import frc.robot.subsystems.elevator.ElevatorConstants;
+
+import static edu.wpi.first.units.Units.*;
 
 public class ElevatorIOSim implements ElevatorIO {
+    private final ElevatorSim   elevatorSim;
+    private final PIDController pid;
 
     private boolean running;
-    private ElevatorSim elevatorSim;
-    private PIDController PID;
 
     public ElevatorIOSim() {
-
         //Creating sim elevator object
         elevatorSim = new ElevatorSim(
-            DCMotor.getFalcon500(2), 
-            ElevatorConstants.GEARING, 
-            ElevatorConstants.ELEVATOR_MASS.in(Kilograms), 
-            ElevatorConstants.ELEVATOR_DRUMRADIUS.in(Inches), 
-            ElevatorConstants.ELEVATOR_MIN_HEIGHT.in(Inches), 
-            ElevatorConstants.ELEVATOR_MAX_HEIGHT.in(Inches), 
-            true, 
-            ElevatorConstants.ELEVATOR_MIN_HEIGHT.in(Inches)
+                DCMotor.getFalcon500(2),
+                ElevatorConstants.GEARING,
+                ElevatorConstants.ELEVATOR_MASS.in(Kilograms),
+                ElevatorConstants.ELEVATOR_DRUM_RADIUS.in(Inches),
+                ElevatorConstants.ELEVATOR_MIN_HEIGHT.in(Inches),
+                ElevatorConstants.ELEVATOR_MAX_HEIGHT.in(Inches),
+                true,
+                ElevatorConstants.ELEVATOR_MIN_HEIGHT.in(Inches)
         );
-        PID = new PIDController(ElevatorConstants.SIM_PID.kP, ElevatorConstants.SIM_PID.kI, ElevatorConstants.SIM_PID.kD);
+        pid = new PIDController(
+                ElevatorConstants.SIM_PID.kP, ElevatorConstants.SIM_PID.kI, ElevatorConstants.SIM_PID.kD);
         running = false;
-
     }
 
     /**
-     * Set voltage of the motor to assigned voltage 
-     *@param voltage voltage value
+     * Set voltage of the motor to assigned voltage
+     *
+     * @param voltage voltage value
      */
     @Override
     public void setMotorVoltage(double voltage) {
@@ -57,16 +54,18 @@ public class ElevatorIOSim implements ElevatorIO {
 
     /**
      * sets the elevator to the assigned setpoint
+     *
      * @param setpoint setpoint value
      */
     @Override
     public void setSetpoint(Distance setpoint) {
-        PID.setSetpoint(setpoint.in(Meters));
+        pid.setSetpoint(setpoint.in(Meters));
         running = true;
     }
 
     /**
      * returns the elevator encoders value
+     *
      * @return values of the encoder
      */
     @Override
@@ -80,13 +79,13 @@ public class ElevatorIOSim implements ElevatorIO {
         double current = 0.0;
         if (running) {
             volts = MathUtil.clamp(
-                PID.calculate(elevatorSim.getPositionMeters()) * 12, 
-                -RoboRioSim.getVInVoltage(), 
-                RoboRioSim.getVInVoltage()
+                    pid.calculate(elevatorSim.getPositionMeters()) * 12,
+                    -RoboRioSim.getVInVoltage(),
+                    RoboRioSim.getVInVoltage()
             );
             current = elevatorSim.getCurrentDrawAmps();
         }
-        elevatorSim.setInputVoltage (volts);
+        elevatorSim.setInputVoltage(volts);
         elevatorSim.update(0.02);
 
         inputs.isMainMotorConnected = true;
