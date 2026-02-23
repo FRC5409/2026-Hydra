ad91020990c59ea6af2f42fea00cd4c478f707ff
chzboi42 <1rajanjad@hdsb.ca>
2026-02-22 21:26:07 -0500

    Added rotation converter constants (pinion rotations still unknown)

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -18,10 +18,12 @@ public final class HopperConstants {
     public static final int FOLLOWER_MOTOR_ID = 21;
 
     public static final Current CURRENT_LIMIT = Amps.of(30.0);
-    public static final double kGearing = 10.0 / 1.0;
+    public static final double kGearing = 12.0 / 1.0;
+    private static final double totalPinionRotations = 1; //Need values for rack and pinion
+    public static final Distance DistancePerPinionRotation = Inches.of(12 / totalPinionRotations);
+    public static final double kRotationConverter = DistancePerPinionRotation.in(Inches) / kGearing;
+
     public static final Distance HOPPER_DRUMRADIUS = Meters.of(1.0);
-    public static final double kCircumfrence = 2 * Math.PI * HOPPER_DRUMRADIUS.in(Inches);
-    public static final double kRotationConverter = kCircumfrence / kGearing; //Need values for rack & pinion
     public static final Mass HOPPER_MASS = Pounds.of(10.561);
 
     public static final PIDConstants TALONFX_PID = new PIDConstants(0.001, 0, 0);
