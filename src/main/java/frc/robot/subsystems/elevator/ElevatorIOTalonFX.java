5543059fdd3aeecc16db6e3b76f9ee9b9873fdf8
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2026-02-26 12:23:56 -0500

    updated elevator torque current logging

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
@@ -38,6 +38,7 @@ public class ElevatorIOTalonFX implements ElevatorIO {
     private StatusSignal<Voltage> mainMotorVoltage;
     private StatusSignal<Current> mainMotorCurrent;
     private StatusSignal<Temperature> mainMotorTemp;
+    private StatusSignal<Current> mainMotorTorqueCurrent;
 
 
     public ElevatorIOTalonFX(int mainMotorID) {
@@ -74,6 +75,7 @@ public class ElevatorIOTalonFX implements ElevatorIO {
         mainMotorVoltage = m_motor.getMotorVoltage();
         mainMotorCurrent = m_motor.getSupplyCurrent();
         mainMotorTemp  = m_motor.getDeviceTemp();
+        mainMotorTorqueCurrent = m_motor.getTorqueCurrent();
 
         // Update all the values
         BaseStatusSignal.setUpdateFrequencyForAll(
@@ -81,7 +83,8 @@ public class ElevatorIOTalonFX implements ElevatorIO {
             motorPosition,
             mainMotorVoltage,
             mainMotorCurrent,
-            mainMotorTemp
+            mainMotorTemp,
+            mainMotorTorqueCurrent
         );
 
         m_motor.optimizeBusUtilization();
@@ -143,11 +146,11 @@ public class ElevatorIOTalonFX implements ElevatorIO {
             mainMotorCurrent, 
             mainMotorTemp
         ).isOK();
-        inputs.mainAppliedVoltage = Units.Volts.of(mainMotorVoltage.getValueAsDouble());
-        inputs.mainAppliedCurrent = Units.Amps.of(Math.abs(mainMotorCurrent.getValueAsDouble()));
+        inputs.mainAppliedVoltage = mainMotorVoltage.getValue();
+        inputs.mainAppliedCurrent = mainMotorCurrent.getValue();
         inputs.mainMotorTemperature = mainMotorTemp.getValueAsDouble();
         inputs.mainMotorPosition = Units.Meters.of(motorPosition.getValueAsDouble());
-        inputs.mainMotorTorqueCurrent = m_motor.getTorqueCurrent().getValue();
+        inputs.mainMotorTorqueCurrent = mainMotorTorqueCurrent.getValue();
         
     }
 }
