83a94430134c5bf126ec685a661637184bf2c584
chzboi42 <1rajanjad@hdsb.ca>
2026-03-01 18:38:07 -0500

    Removed Amps import from Hopper.java

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -2,7 +2,6 @@
 
 package frc.robot.subsystems.hopper;
 
-import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 
