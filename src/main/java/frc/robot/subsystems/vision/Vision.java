ed38947232b64ef2fffafe7cf694b1b81b647a5a
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2026-03-01 01:26:42 -0500

    updated LL offsets

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -52,10 +52,10 @@ public class Vision extends SubsystemBase {
      */
     public static final double THETA_STDDEV_PER_METER = 1.5;
 
-    // TODO: update these to camera offset
+    // TODO: update these to camera offset - Double check angle
     public static final Transform3d OFFSET_FROM_ROBOT_ORIGIN = new Transform3d(
-                                                                    new Translation3d(Inches.of(0.0), Inches.of(0.0), Inches.of(0.0)),
-                                                                    new Rotation3d( Degrees.of(0.0),  Degrees.of(0.0),   Degrees.of(0.0))
+                                                                    new Translation3d(Inches.of(-11.639), Inches.of(- 1.623 ), Inches.of(20.027)),
+                                                                    new Rotation3d( Degrees.of(0.0),  Degrees.of(5.0),   Degrees.of(0.0))
                                                                 );
 
     private final VisionIO               io;
