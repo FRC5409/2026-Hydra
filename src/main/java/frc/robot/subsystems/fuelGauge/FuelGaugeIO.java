566f2f57c37ccf640350eced5ab8b8943dcaeca4
reneerenko <1titarenkren@hdsb.ca>
2026-03-03 16:51:30 -0500

    Update FuelGaugeIO.java

--- src/main/java/frc/robot/subsystems/fuelGauge/FuelGaugeIO.java
+++ src/main/java/frc/robot/subsystems/fuelGauge/FuelGaugeIO.java
@@ -15,5 +15,7 @@ public interface FuelGaugeIO {
 
     default Voltage getVoltage() {return Volts.of(0.0);}
 
+    default Voltage getVoltage() {return Volts.of(0.0);}
+
     default void updateInputs(FuelGaugeInputs inputs) {}
 }
