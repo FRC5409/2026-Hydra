db41d980d5d2003643672da7b8a0f56cff350565
Logan Dhillon <dev@logandhillon.com>
2026-03-11 15:37:08 -0400

    chore: update hopper gearing

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -8,14 +8,13 @@ import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Mass;
 
-/* ALL TEST VALUES */
 public final class HopperConstants {
 
     public static final Current CURRENT_LIMIT = Amps.of(20.0);
     public static final Current DAMAGE_DETECTION_CURRENT = Amps.of(50.0); //TODO: test on real robot
     public static final boolean CRASH_DETECTION_ENABLED = true;
 
-    public static final double GEARING = 12.0 / 1.0;
+    public static final double GEARING = 9.0 / 1.0;
 
     public static final Distance HOPPER_DRUMRADIUS = Meters.of(1.0);
     public static final Mass HOPPER_MASS = Pounds.of(10.561);    
