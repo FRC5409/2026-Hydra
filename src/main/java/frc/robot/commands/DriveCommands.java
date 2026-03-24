3c283d8c30ea6138447afa38f3aaff5408ee7664
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2026-03-24 14:10:31 -0400

    Updated rotation speed modifier according to Mr. Szura's recommendation

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -67,7 +67,7 @@ public class DriveCommands {
   private static final double WHEEL_RADIUS_MAX_VELOCITY = 0.25; // Rad/Sec
   private static final double WHEEL_RADIUS_RAMP_RATE = 0.05; // Rad/Sec^2
   private static double translationSpeedModifier = 1.0;
-  private static double rotationSpeedModifier = 0.8;
+  private static double rotationSpeedModifier = 0.5;
 
   private static boolean isAligned = false;
 
