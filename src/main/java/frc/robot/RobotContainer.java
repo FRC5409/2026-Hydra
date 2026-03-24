7b6842aa93ac9afdbe33a4d526b360fea5d120ef
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2026-03-24 19:06:44 -0400

    Added drive brake/coast

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -482,6 +482,10 @@ public class RobotContainer {
 
         SmartDashboard.putData("Intake/Coast", sys_intake.coastMode().ignoringDisable(true)); // TODO: REMOVE WHEN MAIN
         SmartDashboard.putData("Intake/Brake", sys_intake.brakeMode().ignoringDisable(true)); // TODO: REMOVE WHEN MAIN
+    
+        SmartDashboard.putData("Drive/Coast", Commands.runOnce(() -> sys_drive.coastMode()).ignoringDisable(true));
+        SmartDashboard.putData("Drive/Brake", Commands.runOnce(() -> sys_drive.brakeMode()).ignoringDisable(false));
+
     }
 
     private Command prepClimberPositionCommand(ClimbingPositions climbingPosition) {
