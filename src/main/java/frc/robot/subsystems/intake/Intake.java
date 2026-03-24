69e137ec60e3a1f1da753657ae6837fa8f282e98
chzboi42 <1rajanjad@hdsb.ca>
2026-03-24 17:47:54 -0400

    Intake: Moved extension setpoint logic to extend() Command
    Moved extension logic from old setSetpoint() to extend()

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -114,7 +114,14 @@ public class Intake extends SubsystemBase {
      * @return A command that extends the intake when executed.
      */
     public Command extend() {
-        return setIntakeSetpoint(() -> Extension.EXTENSION_DISTANCE);
+        return Commands.sequence(
+            Commands.runOnce(() -> io.setSetpoint(IntakeConstants.Extension.EXTENSION_MAX_DISTANCE)),
+            Commands.waitTime(Milliseconds.of(500)),
+            Commands.run(() -> {
+                if (io.getSetpoint().gte(io.getPosition()))
+                    io.setSetpoint(io.getPosition());
+            }).until(() -> io.getSetpoint().gte(io.getPosition()))
+        ).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
     }
 
     /**
@@ -124,17 +131,6 @@ public class Intake extends SubsystemBase {
      *
      * @return A command that moves the intake to the specified position when executed.
      */
-    public Command setIntakeSetpoint(Supplier<Distance> setpoint) {
-        return Commands.sequence(
-            Commands.runOnce(() -> io.setSetpoint(Meters.of(setpoint.get().in(Meters)))),
-            Commands.waitTime(Milliseconds.of(500)),
-            Commands.run(() -> {
-                if (io.getSetpoint().gte(io.getPosition()))
-                    io.setSetpoint(io.getPosition());
-            }).until(() -> io.getSetpoint().gte(io.getPosition()))
-        ).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
-    }
-
     public Command setSetpoint(Supplier<Distance> setpoint){
         return Commands.runOnce(() -> io.setSetpoint(setpoint.get()));
     }
