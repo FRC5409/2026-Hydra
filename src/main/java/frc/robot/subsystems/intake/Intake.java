84de05f56bd310930d6a72b80359763fea019506
chzboi42 <1rajanjad@hdsb.ca>
2026-03-24 15:17:54 -0400

    Re-updated Intake Extension distances + setSetpoint

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -114,7 +114,7 @@ public class Intake extends SubsystemBase {
      * @return A command that extends the intake when executed.
      */
     public Command extend() {
-        return Commands.runOnce(() -> io.setSetpoint(Extension.EXTENSION_DISTANCE));
+        return setIntakeSetpoint(() -> Extension.EXTENSION_DISTANCE);
     }
 
     /**
@@ -124,15 +124,19 @@ public class Intake extends SubsystemBase {
      *
      * @return A command that moves the intake to the specified position when executed.
      */
-    public Command setSetpoint(Supplier<Distance> setpoint) {
+    public Command setIntakeSetpoint(Supplier<Distance> setpoint) {
         return Commands.sequence(
             Commands.runOnce(() -> io.setSetpoint(Meters.of(setpoint.get().in(Meters)))),
-            Commands.waitTime(Milliseconds.of(800)),
+            Commands.waitTime(Milliseconds.of(500)),
             Commands.run(() -> {
-                if (MathUtils.withinTolerance(io.getSetpoint().in(Meters), io.getPosition().in(Meters), 5))
+                if (io.getSetpoint().gte(io.getPosition()))
                     io.setSetpoint(io.getPosition());
-            }).until(() -> MathUtils.withinTolerance(io.getSetpoint().in(Meters), io.getPosition().in(Meters), 5))
+            }).until(() -> io.getSetpoint().gte(io.getPosition()))
         ).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
+    }
+
+    public Command setSetpoint(Supplier<Distance> setpoint){
+        return Commands.runOnce(() -> io.setSetpoint(setpoint.get()));
     }
 
     /**
