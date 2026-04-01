2f9cdcbaeb3fa4518611675a4c1fc391f96861bd
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2026-04-01 17:31:24 -0400

    Update gyro in sim

--- src/main/java/frc/robot/subsystems/drive/GyroIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOSim.java
@@ -4,6 +4,8 @@ import static edu.wpi.first.units.Units.*;
 
 import org.ironmaple.simulation.drivesims.GyroSimulation;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class GyroIOSim implements GyroIO {
 
     private final GyroSimulation gyroSim;
@@ -20,5 +22,15 @@ public class GyroIOSim implements GyroIO {
         inputs.yawVelocityRadPerSec = RadiansPerSecond.of(gyroSim.getMeasuredAngularVelocity().in(RadiansPerSecond));
         inputs.odometryYawTimestamps = new double[] {};
         inputs.odometryYawPositions = gyroSim.getCachedGyroReadings();
+    }
+
+    @Override
+    public  void zeroPigeon(){
+        setPigeonYaw(Rotation2d.k180deg);
+    }
+
+    @Override
+    public  void setPigeonYaw(Rotation2d rotation2d){
+        gyroSim.setRotation(rotation2d);
     }
 }
